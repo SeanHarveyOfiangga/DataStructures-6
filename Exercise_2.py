3ebc12a139636375d82aab121b47c44ae2b5e3eb
unknown <seanharveyofiangga@gmail.com>
2023-01-14 23:07:59 +0800

    coded deleted function

--- Exercise_2.py
+++ Exercise_2.py
@@ -41,3 +41,22 @@ class BinarySearchTreeNode:
             elements += self.right.in_order_traversal()
         return elements
 
+    def delete(self, val):
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.left
+            min_val = self.right.find_min()
+            self.data = min_val
+            self.right = self.right.delete(min_val)
+        return self
+
