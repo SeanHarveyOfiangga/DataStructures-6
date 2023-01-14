4204d4ac7bb6e2f86c75ca4e87551984cc438700
unknown <seanharveyofiangga@gmail.com>
2023-01-14 22:44:48 +0800

    Added sum calculator function

--- Exercise_1.py
+++ Exercise_1.py
@@ -78,6 +78,12 @@ class BinarySearchTreeNode:
         if self.left is None:
             return self.data
         return self.left.find_min()
+    
+    #sum calculator
+    def calculate_sum(self):
+        left_sum = self.left.calculate_sum() if self.left else 0
+        right_sum = self.right.calculate_sum() if self.right else 0
+        return self.data + left_sum + right_sum
 
 #tree building function
 def build_tree(elements):
